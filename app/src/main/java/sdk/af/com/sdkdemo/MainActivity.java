598345af8fe40d8c59bd1c20d7d964bd65c67335
zhiqiang <wzqhbkjdx@126.com>
2018-12-13 19:48:57 +0800

    create prj

---
+++ app/src/main/java/sdk/af/com/sdkdemo/MainActivity.java
@@ -0,0 +1,13 @@
+package sdk.af.com.sdkdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
